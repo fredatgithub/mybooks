07c5349db6c8e86c686df57eaaf5b7a63225d73a
Franck Wehrling <franck.wehrling.external@spherea.com>
2019-10-09 01:11:06 +0200

    preparation du controller pour la creation d'un nouveau livre dans book-form.component.ts

--- src/app/book/book-form/book-form.component.ts
+++ src/app/book/book-form/book-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BookService } from 'src/app/services/book.service';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-book-form',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-form.component.scss']
 })
 export class BookFormComponent implements OnInit {
-
-  constructor() { }
+  bookForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private bookService: BookService, private router: Router) {}
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.bookForm = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      author: ['', [Validators.required]]
+    });
+  }
+
+  onSaveBook() {
+    const title = this.bookForm.get('title').value;
+    const author = this.bookForm.get('author').value;
+    const book = new Book(title, author);
+    this.bookService.createBook(book);
+    this.router.navigate(['/books']);
+  }
 }
