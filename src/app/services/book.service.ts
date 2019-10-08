ba5135cb1dce5eb19124b8cbbf71f523aee15b27
Franck Wehrling <franck.wehrling.external@spherea.com>
2019-10-09 00:33:59 +0200

    preparation des requêtes avec firebase dans book.service.ts

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
+import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  books: Book[] = [];
+  bookSubject = new Subject<Book[]>();
 
-  constructor() { }
+  constructor() {}
+
+  emitBooks() {
+    this.bookSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase
+      .database()
+      .ref('/books')
+      .set(this.books);
+  }
+
+  getBooks() {
+    firebase
+      .database()
+      .ref('/books')
+      .on('value', data => {
+        this.books = data.val() || [];
+        this.emitBooks();
+      });
+  }
+
+  getBook(id: number) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .database()
+        .ref(`/books/${id}`)
+        .once('value')
+        .then(data => resolve(data.val()))
+        .catch(error => reject(error));
+    });
+  }
+
+  createBook(book: Book) {
+    this.books.push(book);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  deleteBook(book: Book) {
+    const indexBook = this.books.findIndex((bookEl: Book) => bookEl === book);
+    this.books.splice(indexBook, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
